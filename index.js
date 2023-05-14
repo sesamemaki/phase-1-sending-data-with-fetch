0f99dcfd9ee2697e3a469837da6aec185317042c
Burcu Bayraö <burju7@yahoo.com>
2023-05-14 10:49:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email) {
+  const userData = {
+    name: "Byron",
+    email: "Poodle@gmail.com",
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+      const p = document.createElement("p");
+      p.innerHTML = `${data.id}`;
+      document.querySelector("body").appendChild(p);
+    })
+    .catch((error) => {
+      document.body.innerHTML = error.message;
+    });
+}
